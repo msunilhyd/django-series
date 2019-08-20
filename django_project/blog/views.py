eeac226806ba42ab0a549cd48b476c84672ececa
msunilhyd <msunilhyd@gmail.com>
2019-08-20 05:23:07 -0700

    20:Aug:19 : Added User Registration and crispy forms

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts' : Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html', {'title':'About Page title'})
